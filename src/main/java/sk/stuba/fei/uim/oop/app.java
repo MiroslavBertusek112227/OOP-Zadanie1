7ba2e41f88aa012af3fc51a49b1668f04c68c326
MiroslavBertusek <xbertusek@stuba.sk>
2022-03-23 22:04:14 +0100

    4. commit

--- src/main/java/sk/stuba/fei/uim/oop/app.java
+++ src/main/java/sk/stuba/fei/uim/oop/app.java
@@ -3,5 +3,18 @@ package sk.stuba.fei.uim.oop;
 public class app {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int c = porovnanie(0, 0);
+        System.out.println(c);
+    }
+    public static int porovnanie(int a, int b) {
+        int c = 0;
+        if (a > b) {
+            c=1;
+        } else if (a==b) {
+            c=0;
+        } else {
+            c=-1;
+        }
+        return c;
     }
 }
